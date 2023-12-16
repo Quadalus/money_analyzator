1ac52df8737e31bb3678ceb68448915450a629d7
quadalus <quadalus@yandex.ru>
2023-12-16 03:12:34 +0300

    feat: добавил docker-compose.yaml

--- mg-analytics/src/main/java/ru/bikkul/analyzator/service/impl/AnalyzerServiceFileImpl.java
+++ mg-analytics/src/main/java/ru/bikkul/analyzator/service/impl/AnalyzerServiceFileImpl.java
@@ -1,6 +1,5 @@
 package ru.bikkul.analyzator.service.impl;
 
-import ch.qos.logback.core.testUtil.StringListAppender;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.EnableScheduling;
